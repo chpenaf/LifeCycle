df22ef45bdfda80776e1d93078a5c159ef0b4527
Christian Peña <ch.penaf@gmail.com>
2022-06-29 23:02:42 -0400

    LifeCycles

---
+++ src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styles: [],
+})
+export class MuestraNombreComponent implements OnInit, OnChanges {
+  constructor() {}
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    
+  }
+
+  ngOnInit(): void {}
+}
---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,72 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [],
+})
+export class Pagina1Component
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+
+  nombre: string = '';
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+
+  onClick(): void {
+
+  }
+
+}
